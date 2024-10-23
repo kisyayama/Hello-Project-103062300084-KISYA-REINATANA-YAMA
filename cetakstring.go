b66cddb068e7c9af2be4dfebffa660cbf08a4ec9
kisyayama <kisyareinatanayama@gmail.com>
2024-10-23 20:41:31 +0700

    Add files via upload

---
+++ cetakstring.go
@@ -0,0 +1,20 @@
+package main // Mulai program
+
+import "fmt" // untuk memanggil program, input output data
+
+func main()  { // fungsi untuk menjalankan program
+	var z int // deklarasi variabel z bilangan bulat non-negatif dengan type integer 
+	var k string // deklarasi variabel k kata yang diulang dengan type string 
+
+	fmt.Println("Perulangan Kata") // cetak kalimat Perulangan Kata
+	fmt.Println("===============================") // cetak simbol =
+	fmt.Print("Masukan Jumlah Kata yang akan diulang :") // cetak kalimat Masukan Jumlah Kata yang akan diulang
+	fmt.Scan(&z) // masukan atau input NILAI kata yang akan diulang
+	fmt.Print("Masukan Kata yang akan diulang :") // cetak kalimat Masukan Kata yang akan diulang
+	fmt.Scan(&k) // masukan atau input KATA yang akan diulang
+
+	for i := 1; i < z; i++ { // looping atau batas pengulangan yang akan dijalankan program
+		fmt.Println(k) // cetak hasil dari banyak kata yang di minta 
+	}
+	
+} // program selesai 
